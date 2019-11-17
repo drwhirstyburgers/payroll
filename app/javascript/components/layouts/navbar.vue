eb3fca1d1c9d0805105b7398f8f90f187fec6264
Drwhirstyburgers <dave.hirst50@gmail.com>
2019-11-16 22:02:01 -0500

    makes some changes to formatting

--- app/javascript/components/layouts/navbar.vue
+++ app/javascript/components/layouts/navbar.vue
@@ -10,24 +10,24 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="/">New Report</a>
+                    <a class="nav-link" href="/payroll_reports">Reports</a>
                 </li>
                 <li class="nav-item active">
-                    <a class="nav-link" href="/employees/new">New Employee</a>
+                    <a class="nav-link" href="/job_groups">Job Groups</a>
                 </li>
                 <li class="nav-item active">
-                    <a class="nav-link" href="/job_groups/new">New Job Group</a>
+                    <a class="nav-link" href="/employees">Employees</a>
                 </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Indexes
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="/payroll_reports">Reports</a>
-                    <a class="dropdown-item" href="/employees">Employees</a>
-                    <a class="dropdown-item" href="/job_groups">Job Groups</a>
-                </div>
-            </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Create a new...
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="/">Report</a>
+                        <a class="dropdown-item" href="/employees/new">Employee</a>
+                        <a class="dropdown-item" href="/job_groups/new">Job Group</a>
+                    </div>
+                </li>
             </ul>
         </div>
     </nav>
