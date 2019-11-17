f8a3c856700a9fe6e05904c50d6903d432feda70
Drwhirstyburgers <dave.hirst50@gmail.com>
2019-11-16 21:47:47 -0500

    formatting

--- app/javascript/components/layouts/navbar.vue
+++ app/javascript/components/layouts/navbar.vue
@@ -9,16 +9,23 @@
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="/">Home<span class="sr-only">(current)</span></a>
-            </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="/">New Report</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="/employees/new">New Employee</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="/job_groups/new">New Job Group</a>
+                </li>
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Navigation
+                    Indexes
                 </a>
                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="/payroll_reports">Report History</a>
+                    <a class="dropdown-item" href="/payroll_reports">Reports</a>
+                    <a class="dropdown-item" href="/employees">Employees</a>
+                    <a class="dropdown-item" href="/job_groups">Job Groups</a>
                 </div>
             </li>
             </ul>
--- app/models/payroll_report.rb
+++ app/models/payroll_report.rb
@@ -1,2 +1,3 @@
 class PayrollReport < ApplicationRecord
+    has_many :rows, dependent: :destroy
 end
