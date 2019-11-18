00325d890243fd0adf710a9ee3e5007718cf9f47
Drwhirstyburgers <dave.hirst50@gmail.com>
2019-11-17 20:44:20 -0500

    adds basic component scaffolding

--- app/javascript/components/layouts/reportsshow.vue
+++ app/javascript/components/layouts/reportsshow.vue
@@ -1,5 +1,14 @@
 <template>
-    
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-3">
+                <reportssidebar :all_reports="reports" />
+            </div>
+            <div class="col-lg-9">
+                <showtable :one_report="selectedReport" />
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -9,7 +18,8 @@ import showtable from '../tables/showtable'
 export default {
     data() {
         return {
-
+            reports: [],
+            selectedReport: []
         }
     },
     components: { reportssidebar, showtable }
